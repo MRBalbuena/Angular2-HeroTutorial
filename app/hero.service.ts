55f960ed5a4beb068c7904b0cfe6a82fdc70b19b
mrbalbuena <mrbalbuena@hotmail.com>
2016-10-05 19:41:09 +0100

    implemented update and create in service.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService{
+    private heroesUrl = 'app/heroes';
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http){}
+
     getHeroe(id: number): Promise<Hero>{
         return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
     }
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
-    };
+
+    getHeroes(): Promise<Hero[]>{
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data  as Hero[])
+            .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero>{
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(response => response.json().data)
+            .catch(this.handleError);
+    }
+
+    update(hero:Hero): Promise<Hero>{
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
     getHeroesSlowly(): Promise<Hero[]> {
     return new Promise<Hero[]>(resolve =>
         setTimeout(resolve, 2000)) // delay 2 seconds
         .then(() => this.getHeroes());
     };
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+
 }
